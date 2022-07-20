d282a3a3766861ae1ca60087307092fbcb1f7c3e
tianjun <tianjunf@digitalchina.com>
2022-07-20 17:39:26 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,23 @@ module.exports = {
     // "plugins": [
     //     "import"
     // ],
-    "parser": "babel-eslint",
+    "parser": "vue-eslint-parser",
     "ignorePatterns": [
         "generated-stories-entry.js",
         "storybook-init-framework-entry.js"
     ],
+    parserOptions: {
+        parser: 'babel-eslint',
+        // ecmaVersion: 'latest',
+        //把 latest 这里修改一下就好了
+        ecmaVersion: 12,
+        sourceType: 'module',
+        allowImportExportEverywhere: true, // 不限制eslint对import使用位置
+        ecmaFeatures: {
+            modules: true,
+            legacyDecorators: true
+        }
+    },
     "plugins": [
         "html",
         "vue"
