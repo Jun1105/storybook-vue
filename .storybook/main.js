ffc0537f816ab20ed2bb54e5c7f3843d028aad28
tianjun <tianjunf@digitalchina.com>
2022-07-20 15:55:59 +0800

    update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,18 @@ module.exports = {
     "@storybook/addon-interactions"
   ],
   "framework": "@storybook/vue",
-  webpackFinal: async (config) => {
-    return { ...config, module: { ...config.module, rules:     custom.module.rules } };
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    });
+
+    // Return the altered config
+    return config;
   },
 }
